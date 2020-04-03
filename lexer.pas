236c8809017112e703c1e0e2065b2d2f97221f51
Robert Roland <rob.roland@gmail.com>
2020-04-02 19:07:39 -0700

    more lexer handler functions

--- lexer.pas
+++ lexer.pas
@@ -26,6 +26,11 @@ type
     function NextToken: TToken;
   private
     procedure SkipWhitespace;
+
+    function HandleComment: TToken;
+    function HandleStringLiteral: TToken;
+    function HandleNumber: TToken;
+    function HandleAlpha: TToken;
   end;
 
 implementation
@@ -43,17 +48,17 @@ procedure TLexer.SkipWhitespace;
 var
   Buffer: array[0..1] of char;
   ReadCount: longint;
-  Pos: Int64;
+  Pos: int64;
 begin
   ReadCount := Stream.Read(Buffer, 1);
   while ReadCount > 0 do
   begin
     case Ord(Buffer[0]) of
-      $0, $9, $0A, $0D: ;
+      $0, $9, $0A, $0D, $20: ;
       else
       begin
         Pos := Stream.Position;
-        dec(Pos);
+        Dec(Pos);
         Stream.Position := Pos;
         exit;
       end;
@@ -63,9 +68,59 @@ begin
 end;
 
 function TLexer.NextToken: TToken;
+var
+  Buffer: array[0..1] of char;
+  ReadCount: longint;
+  Pos: int64;
 begin
   SkipWhitespace;
 
+  ReadCount := Stream.Read(Buffer, 1);
+  if ReadCount < 1 then
+  begin
+    Result.TokenType := EOF;
+    Result.Value := 'EOF';
+    exit;
+  end;
+
+  case Buffer[0] of
+    '0'..'9': Result := HandleNumber;
+    'A'..'Z', 'a'..'z': Result := HandleAlpha;
+    '''': Result := HandleStringLiteral;
+    '"': Result := HandleComment;
+    else
+    begin
+      Pos := Stream.Position;
+      Dec(Pos);
+      Stream.Position := Pos;
+      exit;
+    end;
+  end;
+
+  Result.TokenType := EOF;
+  Result.Value := 'EOF';
+end;
+
+function TLexer.HandleComment: TToken;
+begin
+  Result.TokenType := EOF;
+  Result.Value := 'EOF';
+end;
+
+function TLexer.HandleStringLiteral: TToken;
+begin
+  Result.TokenType := EOF;
+  Result.Value := 'EOF';
+end;
+
+function TLexer.HandleNumber: TToken;
+begin
+  Result.TokenType := EOF;
+  Result.Value := 'EOF';
+end;
+
+function TLexer.HandleAlpha: TToken;
+begin
   Result.TokenType := EOF;
   Result.Value := 'EOF';
 end;
